a86c8496069be7dd7c56fd1dcf3e89ae55a13939
余芃 <2157440342@qq.com>
2019-03-24 14:16:14 +0800

    作业A07

---
+++ A07/源.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+int main() {
+	const double PI = 3.1416;
+		double r, s, c;
+		std::cout << "请输入圆形半径，按回车键结束: ";
+		std::cin >> r;
+		s = PI * r * r;
+		c = PI * 2 * r;
+		std::cout << std::endl;
+		std::cout << "圆形周长：" << r << std::endl;
+		std::cout << "圆形周长：" << c << std::endl;
+		std::cout << "圆形面积：" << s << std::endl;
+		std::cin.get();
+		std::cin.get();
+		return 0;
+}
